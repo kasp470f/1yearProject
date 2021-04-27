be46f1b0ac75d6c92a1c77a8d43d0be2bc44620b
mathex83 <marnor83@gmail.com>
2021-04-27 10:01:51 +0200

    Created ExportWindow

---
+++ TrashHandling/Windows/ExportWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TrashHandling.Windows
+{
+	/// <summary>
+	/// Interaction logic for ExportPopup.xaml
+	/// </summary>
+	public partial class ExportWindow : Window
+	{
+		public ExportWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
