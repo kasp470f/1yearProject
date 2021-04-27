9a380322fbecf07b5017fb66a715135549518697
mathex83 <marnor83@gmail.com>
2021-04-27 10:01:32 +0200

    Created ChangeData window

---
+++ TrashHandling/Windows/ChangeData.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TrashHandling.Windows
+{
+	/// <summary>
+	/// Interaction logic for ChangeDataPopup.xaml
+	/// </summary>
+	public partial class ChangeData : Window
+	{
+		public ChangeData()
+		{
+			InitializeComponent();
+		}
+	}
+}
