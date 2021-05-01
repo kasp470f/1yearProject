aa044eb1dfeb7536b89a2e1e3fbb1879266b6919
Kasper <kasperwissendorf2011@gmail.com>
2021-05-01 02:08:42 +0200

    Changed some comments

--- TrashHandling/Models/ComboBoxSources.cs
+++ TrashHandling/Models/ComboBoxSources.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TrashHandling.Models
 {
-	/// <summary>
-	/// A directory for Units and categories
-	/// <para>Created by Martin</para>
-	/// </summary>
-	public static class ComboBoxSources
+    /// <summary>
+    /// A directory for Units and categories
+    /// <para>Created by Martin</para>
+    /// </summary>
+    public static class ComboBoxSources
 	{
-		//enum for the unitpicker
+		// Enum for the unitpicker
 		public enum Unit
 		{
 			Kg = 1,
@@ -20,18 +16,19 @@ namespace TrashHandling.Models
 			Colli = 3
 		};
 
-		//list for the category picker
-		public static List<string> Categories = new()
-		{
-			"Batterier",
-			"Biler",
-			"Elektronikaffald",
-			"Imprægneret træ",
-			"Inventar",
-			"Organisk affald",
-			"Pap og papir",
-			"Plastemballager",
-			"PVC"
-		};		
-	}
+        // List for the category picker
+        public static List<string> Categories { get => categories; }
+        private static List<string> categories = new()
+        {
+            "Batterier",
+            "Biler",
+            "Elektronikaffald",
+            "Imprægneret træ",
+            "Inventar",
+            "Organisk affald",
+            "Pap og papir",
+            "Plastemballager",
+            "PVC"
+        };
+    }
 }
