a5721611529d117d9a718299073324daa0b80a66
Kasper <kasperwissendorf2011@gmail.com>
2021-05-02 18:21:44 +0200

    Updated universal style

--- TrashHandling/App.xaml.cs
+++ TrashHandling/App.xaml.cs
@@ -1,8 +1,15 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
 
 namespace TrashHandling
 {
     public partial class App : Application
     {
+        private void DatePicker_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DatePicker datePicker = e.Source as DatePicker;
+            datePicker.IsDropDownOpen = true;
+        }
     }
 }
