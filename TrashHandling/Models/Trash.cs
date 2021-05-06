313729d9b9183abc02a780972cf6e80f62b25c57
Kasper <kasperwissendorf2011@gmail.com>
2021-05-06 12:10:50 +0200

    Update Trash.cs

--- TrashHandling/Models/Trash.cs
+++ TrashHandling/Models/Trash.cs
@@ -1,4 +1,7 @@
-﻿namespace TrashHandling.Models
+﻿using System;
+using static TrashHandling.Models.ComboBoxSources;
+
+namespace TrashHandling.Models
 {
     /// <summary>
     /// The class object containing the properties to handle trash
@@ -6,13 +9,26 @@
     /// </summary>
     public class Trash
 	{
-		public int Id { get; set; }
+        public string LocalID { get; set; }
+        public int Id { get; set; }
 		public decimal Amount { get; set; }
 		public int Units { get; set; }
+        public string UnitsText { get => Enum.GetName(typeof(Unit), Units); }
 		public string Category { get; set; }
-		public string Description { get; set; }
+        public string Description { get; set; }
 		public string ResponsiblePerson { get; set; }
 		public int CompanyId { get; set; }
 		public string RegisterTimeStamp { get; set; }
-	}
+
+
+        /// <summary>
+        /// A overriden function to write out the element.
+        /// <para>Created by Kasper</para>
+        /// </summary>
+        /// <returns>A string with all the properties</returns>
+        public override string ToString()
+        {
+            return $"{Id},{Amount},{Units},{Category},{Description},{ResponsiblePerson},{CompanyId},{RegisterTimeStamp}";
+        }
+    }
 }
